554e7f605646463605a098ea143177c10b3604cd
sevenbitscience <36937340+sevenbitscience@users.noreply.github.com>
2024-10-02 09:48:11 -0400

    Rewrite exam-time fetch statement

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-function generate(){
+const exams = "https://raw.githubusercontent.com/vincentdinic/FinalExamScheduleGenerator/refs/heads/main/CRNsAndExams.json";
+
+function generate() {
     // takes in CRN from text box,
-    var div = document.getElementById("crn")
-    var CRN = div.innerHTML;
-    // compares in our JSON file
-    // var time = fetchJSON();
-    const fetchTime = async() => {
-        let time = await fetch("https://raw.githubusercontent.com/vincentdinic/FinalExamScheduleGenerator/refs/heads/main/CRNsAndExams.json");
-        return time.json();
-    };
-    // outputs in some way
-    let time = fetchTime();
-    console.log(time);
-    // for next time, unwrap the promise so we can access individual CRN time.
-    console.log(time.CRN);
+    var CRN = document.getElementById("crn").value;
+	console.log("Finding for CRN");
+	console.log(CRN);
+
+	fetch(exams)
+		.then((response) => response.json())
+		.then((examTimes) => {
+			console.log(examTimes[CRN])
+		});
 }
