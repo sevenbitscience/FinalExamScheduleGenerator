9654f0bb031990343e8b7498d843c8f85a20c96f
sevenbitscience <36937340+sevenbitscience@users.noreply.github.com>
2024-10-02 09:51:03 -0400

    Add function to display results

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const exams = "https://raw.githubusercontent.com/vincentdinic/FinalExamScheduleGenerator/refs/heads/main/CRNsAndExams.json";
 
+let courseCount = 0;
+
 function generate() {
     // takes in CRN from text box,
     var CRN = document.getElementById("crn").value;
@@ -10,5 +12,11 @@ function generate() {
 		.then((response) => response.json())
 		.then((examTimes) => {
 			console.log(examTimes[CRN])
+            display(examTimes[CRN], 0)
 		});
+}
+
+function display(examDateTime, courseNumber) {
+	textElement = document.getElementById("examTime"+courseNumber);
+	textElement.textContent = examDateTime;
 }
