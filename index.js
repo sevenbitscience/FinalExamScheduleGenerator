dee5255f665aed9a655fddbccebf0b8317374e67
joey <milausnic@gmail.com>
2024-10-02 10:24:03 -0400

    Only fetch the JSON once, on page load

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const exams = "https://raw.githubusercontent.com/vincentdinic/FinalExamScheduleGenerator/refs/heads/main/CRNsAndExams.json";
-
-let courseCount = 0;
+// URL to fetch to the JSON CRN exam-time pairs
+const examsJsonUrl = "https://raw.githubusercontent.com/vincentdinic/FinalExamScheduleGenerator/refs/heads/main/CRNsAndExams.json";
+// Initiallize the var to hold the JSON data
+//
+// Potentially problematic if user tries to search json
+// before it is downloaded and parsed, for now we 
+// assume that is impossible
+var CRNsAndExams = '';
+// Fetch the data and store it in the above variable
+fetch(examsJsonUrl)
+	.then((response) => response.json())
+	.then((examTimes) => {
+		CRNsAndExams= examTimes
+	});
 
 function generate() {
-    // takes in CRN from text box,
-    var CRN = document.getElementById("crn").value;
+	console.log("Finding course " + 0);
+	// takes in CRN from text box,
+	let CRN = document.getElementById("crn"+0).value;
 	console.log("Finding for CRN");
 	console.log(CRN);
-
-	fetch(exams)
-		.then((response) => response.json())
-		.then((examTimes) => {
-			console.log(examTimes[CRN])
-            display(examTimes[CRN], 0)
-		});
+	console.log(CRNsAndExams[CRN]);
+	display(CRNsAndExams[CRN], 0);
 }
 
 function display(examDateTime, courseNumber) {
-	textElement = document.getElementById("examTime"+courseNumber);
+	let textElement = document.getElementById("examTime"+courseNumber);
 	textElement.textContent = examDateTime;
 }
